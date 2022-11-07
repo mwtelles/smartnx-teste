c4df47a2110835e3df8f6fb61f0be420a8a74dab
Matheus Telles <business.mwtelles@gmail.com>
2022-11-07 00:27:21 -0300

    filtrando os personagens pelo nome

--- src/components/characters/index.tsx
+++ src/components/characters/index.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 
 import { Api } from "../../services/api/axios-config";
 
-import { Table } from "antd";
+import { Table, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import "antd/dist/antd.css";
 
 const Characters: React.FC = () => {
-
   const [loading, setLoading] = useState(false);
 
   const [page, setPage] = useState(1);
@@ -16,79 +16,99 @@ const Characters: React.FC = () => {
 
   const [characters, setCharacters] = useState([]);
 
-
   const columns = [
     {
       key: "1",
       title: "Nome",
       dataIndex: "name",
+      filterDropdown: ({
+        setSelectedKeys, selectedKeys, confirm
+      }) => {
+        return (
+          <div>
+            <Input autoFocus placeholder="Pesquise pelo nome"
+            onPressEnter={() => {
+              confirm();
+            }}
+            onBlur={() => {
+              confirm();
+            }}
+            onChange={(e) => {
+              setSelectedKeys(e.target.value ? [e.target.value] : []);
+            }}
+            value={selectedKeys[0]}
+            ></Input>
+          </div>
+        );
+      },
+      filterIcon: () => {
+        return <SearchOutlined />;
+      },
+      onFilter:(value, record) => {
+        return record.name.toLowerCase().includes(value.toLowerCase());
+      }
     },
     {
       key: "2",
       title: "Gênero",
       dataIndex: "gender",
-      filters:[
-        {text: 'male', value:'male'},
-        {text: 'female', value:'female'},
-        {text: 'n/a', value:'n/a'},
+      filters: [
+        { text: "male", value: "male" },
+        { text: "female", value: "female" },
+        { text: "n/a", value: "n/a" },
       ],
       onFilter: (value, record) => {
-        return record.gender === value
-      }
+        return record.gender === value;
+      },
     },
     {
       key: "3",
       title: "Altura",
       dataIndex: "height",
-      sorter:(alturaAsc, alturaDes) => {
-        return alturaAsc.height > alturaDes.height
-      }
+      sorter: (alturaAsc, alturaDes) => {
+        return alturaAsc.height > alturaDes.height;
+      },
     },
     {
       key: "4",
       title: "Peso",
       dataIndex: "mass",
-      sorter:(pesoAsc, pesoDes) => {
-        return pesoAsc.mass > pesoDes.mass
-      }
+      sorter: (pesoAsc, pesoDes) => {
+        return pesoAsc.mass > pesoDes.mass;
+      },
     },
-  ]
+  ];
 
   useEffect(() => {
-
     setLoading(true);
 
     Api.get(`/people/`)
-      .then(response => {
+      .then((response) => {
         setCharacters(response.data.results);
       })
       .catch((err) => console.log(err))
-      .finally(() => setLoading(false))
+      .finally(() => setLoading(false));
   }, []);
-
 
   return (
     <div>
       <Table
-      loading={loading}
-      columns={columns}
-      dataSource={characters}
-      pagination={{
-        current:page,
-        pageSize: pageSize,
-        total: characters.length,
-        showQuickJumper: true,
-        onChange: (page, pageSize) => {
-          setPage(page);
-          setPageSize(pageSize);
-        }
-
-      }}
+        loading={loading}
+        columns={columns}
+        dataSource={characters}
+        pagination={{
+          current: page,
+          pageSize: pageSize,
+          total: characters.length,
+          showQuickJumper: true,
+          onChange: (page, pageSize) => {
+            setPage(page);
+            setPageSize(pageSize);
+          },
+        }}
       />
     </div>
   );
 };
 
 export default Characters;
-
-
